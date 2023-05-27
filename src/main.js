9732622ccb5b36e6862276ee07008fbe014e060f
Andres Camilo Ocampo <andres_ocampo82211@elpoli.edu.co>
2023-05-27 18:16:20 -0500

    CORS

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
+import cors from 'cors'
 
-createApp(App).use(router,VueAxios,axios).mount('#app')
+createApp(App).use(router,VueAxios,axios,cors).mount('#app')
